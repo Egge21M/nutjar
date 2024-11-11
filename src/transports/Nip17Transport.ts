c7e1c195da4e4596acbb7dcc6ae5c83eb39b5b74
Egge <egge@getcurrent.io>
2024-11-11 09:27:24 +0100

    removed verbose (for now)

--- src/transports/Nip17Transport.ts
+++ src/transports/Nip17Transport.ts
@@ -1,15 +1,10 @@
-import {
-  Event,
-  EventTemplate,
-  finalizeEvent,
-  generateSecretKey,
-  SimplePool,
-} from "nostr-tools";
+import { Event, generateSecretKey, SimplePool } from "nostr-tools";
+import * as nip17 from "nostr-tools/nip17";
 import { Transport } from "../types";
 import { Proof } from "@cashu/cashu-ts";
-import { sumProofs } from "@cashu/cashu-ts/dist/lib/es5/utils";
+import { getEncodedToken } from "@cashu/cashu-ts/dist/lib/es5/utils";
 
-export class NutZapTransport implements Transport {
+export class Nip17Transport implements Transport {
   private readonly pool: SimplePool;
   private readonly relays: string[];
   // private readonly verbose?: boolean;
@@ -23,23 +18,16 @@ export class NutZapTransport implements Transport {
   }
 
   async send(proofs: Proof[], mintUrl: string, pubkey: string, memo?: string) {
-    const amountInSats = sumProofs(proofs);
-    const eventTemplate: EventTemplate = {
-      kind: 9321,
-      content: memo || "",
-      created_at: Math.floor(Date.now() / 1000),
-      tags: [
-        ["amount", String(amountInSats)],
-        ["unit", "sat"],
-        ["proof", JSON.stringify(proofs)],
-        ["u", mintUrl],
-        ["p", pubkey],
-      ],
-    };
+    const encodedToken = getEncodedToken({ proofs, mint: mintUrl, memo });
     const randomSk = generateSecretKey();
-    const event = finalizeEvent(eventTemplate, randomSk);
-    console.log(event);
-    const res = await Promise.allSettled(this.publishEvent(event));
+    const wrap = nip17.wrapEvent(
+      randomSk,
+      { publicKey: pubkey },
+      JSON.stringify(encodedToken),
+    );
+    console.log(wrap);
+    const res = await Promise.allSettled(this.publishEvent(wrap));
+    console.log(res);
     if (!res.some((p) => p.status === "fulfilled")) {
       throw new Error("Publishing failed...");
     }
